3af45369b79d7319055ce4f1bff6d078038c685e
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-02-08 21:00:36 -0300

    mapbox

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import mapboxgl from 'mapbox-gl'
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibXJ3aWx2IiwiYSI6ImNsZHc5bmNvdzA1OGczcHFmb3pxMmphemQifQ._Xg8PGF33M-DbfCb0VpT6g';
 
 export const MapPage = () => {
-  return (
-    <div>MapPage</div>
-  )
+
+    const mapDiv = useRef();
+
+    const [, setMapa] = useState();
+
+    const startingPoint = {
+        lng: 5,
+        lat:34,
+        zoom:10,
+    }
+
+    useEffect(() => {
+        const map = new mapboxgl.Map({
+            container: mapDiv.current,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            zoom:startingPoint.zoom,
+            center:[startingPoint.lng,startingPoint.lat]
+        });
+
+        setMapa(map);
+
+    }, [])
+
+
+    return (
+        <>
+            <div
+                className="mapContainer"
+                ref={mapDiv}
+            />
+
+        </>
+    )
 }
